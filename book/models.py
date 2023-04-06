6e1998325f377e0ec273a6561266588310f76d27
Paul Starodub <amorallex@gmail.com>
2023-04-06 10:10:46 +0400

    Updated models.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """Book model"""
+
+    class Enum(models.TextChoices):
+        HARD = "hard"
+        SOFT = "soft"
+
+    title = models.CharField(max_length=63)
+    author = models.CharField(max_length=128)
+    cover = models.CharField(max_length=4, choices=Enum.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
