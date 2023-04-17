2fa2a8c12f549a57fcd9132655dfbe693870aaa9
Paul Starodub <amorallex@gmail.com>
2023-04-17 18:04:01 +0400

    Updated tests.py for book

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+BOOKS_URL = reverse("library:book-list")
+
+
+class UnAuthenticatedBookApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_auth_required(self) -> None:
+        response = self.client.get(BOOKS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
