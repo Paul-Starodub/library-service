303f668f2f449c472773f12a219d1dafe9c1b561
Paul Starodub <amorallex@gmail.com>
2023-04-26 14:43:51 +0400

    Updated models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -89,8 +89,8 @@ class Payment(models.Model):
     borrowing = models.ForeignKey(
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
-    session_url = models.URLField(max_length=250, null=True, blank=True)
-    session_id = models.CharField(max_length=250, null=True, blank=True)
+    session_url = models.URLField(max_length=500, null=True, blank=True)
+    session_id = models.CharField(max_length=500, null=True, blank=True)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
