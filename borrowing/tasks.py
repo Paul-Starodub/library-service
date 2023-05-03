76b88351f1e96935fdb61c0242977c414d0f95e7
Paul Starodub <amorallex@gmail.com>
2023-05-03 19:55:10 +0400

    Updated tasks.py

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -9,6 +9,6 @@ def run_sync_with_api() -> None:
     message = filtering_borrowing()
     if len(message) > 4096:
         for char in range(0, len(message), 4096):
-            borrowing_telegram_notification(message=message[char : char + 4096])
+            borrowing_telegram_notification(message=message[char: char + 4096])
         else:
             borrowing_telegram_notification(message=message)
