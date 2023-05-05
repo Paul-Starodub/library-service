bc3b7d5ccf8f40b340b9f6cf3f7d0f3c9fd422d4
Paul Starodub <amorallex@gmail.com>
2023-05-05 14:30:03 +0400

    Updated book/serializers.py with validation

--- book/serializers.py
+++ book/serializers.py
@@ -11,8 +11,7 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
 
-    @staticmethod
-    def validate_daily_fee(value) -> ValidationError | Decimal:
+    def validate_daily_fee(self, value: Decimal) -> ValidationError | Decimal:
         if value < 0:
             raise serializers.ValidationError(
                 "Borrowing cost cannot be less than zero!"
