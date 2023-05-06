fc2f42541e0705d922ece57e8317c09f8fe705e1
Paul Starodub <amorallex@gmail.com>
2023-05-06 08:36:52 +0300

    Updated tasks.py for asyncio

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -1,10 +1,12 @@
+import asyncio
+
 from celery import shared_task
 
 from borrowing.monitoring import filtering_borrowing
-from borrowing.telegram_notification import borrowing_telegram_notification
+from borrowing.telegram_notification import send_message
 
 
 @shared_task
 def run_sync_with_api() -> None:
     message = filtering_borrowing()
-    borrowing_telegram_notification(message=message)
+    asyncio.run(send_message(message=message))
