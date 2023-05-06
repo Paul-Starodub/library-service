b0b651b26c6b253da76c70ac68d6e03e8929b04b
Paul Starodub <amorallex@gmail.com>
2023-05-06 08:49:30 +0300

    Updated settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -30,15 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
-
-if DEBUG:
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
-        "127.0.0.1",
-        "10.0.2.2",
-    ]
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
